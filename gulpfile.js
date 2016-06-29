bbe5c543b5412e041e76400faddb896090155918
tui2tone <tui2tone@gmail.com>
2016-06-30 00:38:13 +0700

    Add Play and Fullscreen Icons

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,17 @@ var gulp = require("gulp"),
     sass = require("gulp-sass"),
     notify = require("gulp-notify"),
     plumber = require('gulp-plumber'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    port: 3200,
+    server: {
+      baseDir: "./"
+    }
+  });
+});
 
 gulp.task("scss", function() {
   return gulp.src("src/**/*.scss")
@@ -15,10 +25,11 @@ gulp.task("scss", function() {
     .pipe(sass().on('error', function(err) { console.log(err) }))
     .pipe(concat('animaticons.css'))
     .pipe(gulp.dest('./bin/css/'))
+    .pipe(browserSync.stream())
 });
 
 gulp.task("watch", function() {
-  gulp.watch(['./src/**/*.sass'], ['scss']);
+  gulp.watch(['./src/**/*.scss'], ['scss']);
 })
 
-gulp.task("dev", ["scss", "watch"])
+gulp.task("dev", ["scss","browser-sync", "watch"])
